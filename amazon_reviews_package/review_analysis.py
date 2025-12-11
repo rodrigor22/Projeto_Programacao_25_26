e5ad3895d7140d7ab50ad880a550ee3ffefadc9c
rodrigor22 <rodrigomiguelpr22@gmail.com>
2025-12-11 13:07:08 +0000

    Initial commit

--- amazon_reviews_package/review_analysis.py
+++ amazon_reviews_package/review_analysis.py
@@ -1,39 +1,37 @@
 # -*- coding: utf-8 -*-
-"""Calcula a média de todos os scores (pontuações) na lista de reviews fornecida."""
+"""Este ficheiro analisa as reviews, calculando as estatísticas das mesmas, como média dos scores através da função "media_avaliações" e a utilidade média de todas as reviews coma função "calcular_utilidade_media"."""
+
+"""Esta função calcula a média de todos os scores (pontuações) na lista de reviews fornecida."""
 #A função recebe a lista criada pela função criada no ficheiro "data_loader.py"
 def media_avaliacoes(dados):
     if not dados:
         return 0.0
-
     # Criamos uma lista através de uma list comprehension apenas dos scores em cada dicionário
     scores = [review["Score"] for review in dados]
-
     total_score = sum(scores)
     #A funcão dá-nos a média dos scores somando os valores todos de scores com o "sum(scores)" e divindo pela quantidade de scores existente na lista dado pelo "len(scores)"
     return total_score / len(dados)
 
-
-def calcular_helpfulness_medio(dados):
-    """
-    Calcula a utilidade média (helpfulness ratio) de todas as reviews.
-    """
-    if not dados:
-        return 0.0
-
+"""Esta função calcula a utilidade média de todas as reviews"""
+def calcular_utilidade_media(dados):
     total_helpfulness_ratio = 0.0
     valid_reviews_count = 0
-
     for review in dados:
         numerator = review["HelpfulnessNumerator"]
         denominator = review["HelpfullnessDenominator"]
 
         if denominator > 0:
-            ratio = numerator / denominator
-            total_helpfulness_ratio += ratio
+            utilidade_da_review = numerator / denominator
+            # O "total_helpfulless_ratio" simboliza o sumatório de review["HelpfulnessNumerator"]/review["HelpfullnessDenominator"]
+            total_helpfulness_ratio += utilidade_da_review
+            # O "valid_reviews_count" simboliza o número de reviews válidas (que o denomidanor não seja igual a 0)
             valid_reviews_count += 1
-
+        # Ignorar reviews quando review["HelpfullnessDenominator"] == 0
+        else:
+            pass
     if valid_reviews_count > 0:
-        return total_helpfulness_ratio / valid_reviews_count
+        utilidade_media = total_helpfulness_ratio / valid_reviews_count
+        return utilidade_media
     else:
+        # Garante que um valor é sempre devolvido
         return 0.0
-
